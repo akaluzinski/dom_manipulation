525995047eaabff4fdb5f78bd38a2dfbdb161efb
akaluzinski <adrian@kaluzinski.eu>
2020-07-28 18:19:18 +0200

    Detect browser agent & dark mode

--- input_filter.js
+++ input_filter.js
@@ -37,9 +37,27 @@ textAreaElement.addEventListener('input', e => {
 });
 
 
+const isIE = () => {
+    const ua = window.navigator.userAgent;
+    return !!document.documentMode || ua.indexOf('MSIE') > -1 || ua.indexOf('Trident') > -1;
+};
+
+const isMac = /Mac|iPod|iPhone|iPad/.test(navigator.platform);
+
 const isBrowser = typeof window === 'object' && typeof document === 'object';
+
+const isMobile = () => {
+    const match = window.matchMedia('(pointer:coarse)');
+    return match && match.matches || /Android|BlackBerry|iPad|iPod|iPhone|webOS/i
+        .test(navigator.userAgent);
+};
+
+const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 console.log(isBrowser);
 
-
 document.getElementById('isBrowser').innerHTML = `${(typeof window === 'object' && typeof document === 'object')}`;
+document.getElementById('isInternetExplorer').innerHTML = `${isIE()}`;
+document.getElementById('isMac').innerHTML = `${isMac}`;
+document.getElementById('isMobile').innerHTML = `${isMobile()}`;
+document.getElementById('isDarkMode').innerHTML = `${isDarkMode}`;
